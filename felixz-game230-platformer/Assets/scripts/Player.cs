dcaaba839a418f81bdb559de5e68796818bcd690
Zack Felix <09felix26@gmail.com>
2022-02-08 13:01:27 -0800

    Added Controls
    Jumping, Climbing and a rudimentary Double Jump

--- felixz-game230-platformer/Assets/scripts/Player.cs
+++ felixz-game230-platformer/Assets/scripts/Player.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 26.0f;
+    [SerializeField] float jumpSpeed = 26.0f;
+    [SerializeField] float climbSpeed = 26.0f;
+    Collider2D playerCollider; 
     Rigidbody2D playerCharacter; 
     Animator playerAnimator;
+    float gravityScaleAtStart;
+    bool doubleJump = false;
 
     // Start is called before the first frame update
     void Start()
     {
         playerCharacter = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
+        playerCollider = GetComponent<Collider2D>();
+
+        gravityScaleAtStart = playerCharacter.gravityScale;
     }
 
     // Update is called once per frame
@@ -20,6 +28,8 @@ public class Player : MonoBehaviour
     {
         Run();
         FlipSprite();
+        Jump();
+        Climb();
     }
 
     private void Run()
@@ -31,9 +41,11 @@ public class Player : MonoBehaviour
 
         playerAnimator.SetBool("run", true);
 
-        bool moving = Mathf.Abs(playerCharacter.velocity.x) > Mathf.Epsilon;
+        bool hspeed = Mathf.Abs(playerCharacter.velocity.x) > Mathf.Epsilon;
 
-        playerAnimator.SetBool("run", moving);
+        playerAnimator.SetBool("run", hspeed);
+
+        print(runVeloctiy);
         
 
     }
@@ -45,6 +57,70 @@ public class Player : MonoBehaviour
         {
             transform.localScale =  new Vector2(Mathf.Sign(playerCharacter.velocity.x), 1.0f);
         }
+    }
+
+    private void Jump()
+    {
+
+        //Refreshes double jump once contact with ground is made
+        if (playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            doubleJump = true;
+        }
+
+        
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(!playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){
+            //will stop this function if not true
+            
+        
+                if (doubleJump)
+                {
+                    //Function continues, allowing a mid-air jump
+                    playerCharacter.velocity= new Vector2(0.0f, 0.0f);
+                    doubleJump = false;
+                }
+
+                else if (!doubleJump)
+                {
+                
+                    return;
+                }
+            
+        }
+            //Get new Y Velocity based on controllable variable
+            Vector2 jumpVelocity = new Vector2(0.0f, jumpSpeed);
+            playerCharacter.velocity += jumpVelocity;
+        }
+    }
+
+
+
+    private void Climb(){
+        //will stop this function if true
+        if(!playerCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))){
+            
+            playerAnimator.SetBool("climb", false);
+            playerCharacter.gravityScale = gravityScaleAtStart;
+
+            return;
+        }
+
+        //"Vertical" from Input Axes
+        float verticalMove = Input.GetAxis("Vertical");
+        //X needs to remain the same and we need to change Y
+        Vector2 climbVelocity = new Vector2(playerCharacter.velocity.x, verticalMove*climbSpeed);
+        playerCharacter.velocity = climbVelocity;
+        playerCharacter.gravityScale = 0.0f;
+
+        bool vspeed = Mathf.Abs(playerCharacter.velocity.y) > Mathf.Epsilon;
+        playerAnimator.SetBool("climb", vspeed);
+
+        
+
+
     }
 
 
